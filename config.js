15bd895fdd537141622f20b7c860297929aceefa
Galym <almabek.galym@gmail.com>
2017-06-12 13:56:17 +0700

    twitter-bot - make favourited 10 posts with hastag javascript

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+module.exports = {
+  consumer_key: 'hDBjkk6IDVIajffQXZqY0OdHK',
+  consumer_secret: 'ZIGZJ5UYXmgAe7MsizbaA44dgJ9gUYwBzoRauFOik5oBszssUC',
+  access_token_key: '715440960-5fu6lEUhSKjQ5vEiCH0yGpDO451g9wpf7GmJzuO1',
+  access_token_secret: 'VyVxtHVYJVcoekqf4hCv9jYfQkjPMGSoghbDU0TsKoROH'
+}
